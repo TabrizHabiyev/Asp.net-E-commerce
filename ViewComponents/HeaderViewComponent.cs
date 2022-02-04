20427e9be1601ed4d2e9f02a6399fa9193ad1c93
TabrizHabiyev <tabrizfh@code.edu.az>
2022-02-04 17:56:33 +0400

    feat: create header view component

---
+++ ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Asp.net_E_commerce.DAL;
+using Asp.net_E_commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_E_commerce.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private readonly Context _context;
+        public HeaderViewComponent(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ContactDetails contactDetails = _context.contactDetails.FirstOrDefault();
+
+            return View(await Task.FromResult(contactDetails));
+        }
+    }
+}
